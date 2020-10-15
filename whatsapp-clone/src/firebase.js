066c181c3e872c3862c46903875ffc6b2eeaf460
Ritesh <riteshghorpade1996@gmail.com>
2020-10-15 16:46:32 +0530

    Sidebar Componet Added

---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAluRMkGdKJF9xtl4t7AB9sVO5UY2MU8xU",
+    authDomain: "whatsappclone-8e081.firebaseapp.com",
+    databaseURL: "https://whatsappclone-8e081.firebaseio.com",
+    projectId: "whatsappclone-8e081",
+    storageBucket: "whatsappclone-8e081.appspot.com",
+    messagingSenderId: "691509940483",
+    appId: "1:691509940483:web:1f6e2924b382a9dc14d936"
+  };
