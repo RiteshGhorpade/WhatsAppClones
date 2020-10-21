93adc1ca49b4873b85cc335f841dd269971b34c2
Ritesh <riteshghorpade1996@gmail.com>
2020-10-21 11:44:11 +0530

    Firebase Setup

--- whatsapp-clone/src/Chat.js
+++ whatsapp-clone/src/Chat.js
@@ -1,12 +1,68 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
+import { Avatar, IconButton } from "@material-ui/core";
+import AttachFile from "@material-ui/icons/AttachFile";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchOutlined from "@material-ui/icons/SearchOutlined";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
+// import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+// import db from "./firebase";
+// import firebase from "firebase";
 import "./Chat.css";
 
 function Chat() {
+  const [seed, setSeed] = useState("");
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+  const [Input, setInput] = useState("");
+  const sendMessgae = (e) => {
+    e.preventDefault();
+    console.log(Input);
+    setInput("");
+  };
   return (
     <div className="Chat">
-      <div className="Chat_header"></div>
-      <div className="chat_body"></div>
-      <div className="chatfooter"></div>
+      <div className="Chat_header">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <div className="chat_info">
+          <h3>Room Name</h3>
+          <p>Last Seen ...</p>
+        </div>
+        <div className="class_header_roght">
+          <IconButton>
+            <SearchOutlined></SearchOutlined>
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon></MoreVertIcon>
+          </IconButton>
+          <IconButton>
+            <AttachFile></AttachFile>
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat_body">
+        <p className={`chatMessage ${true && "chat-receiver"}`}>
+          <span className="chat__name">Ritesh Ghorpade</span>
+          Hey
+          <span className="chat_timestamp">3:52pm</span>
+        </p>
+        <p className="chatMessage">Hey</p>
+      </div>
+      <div className="chatfooter">
+        <InsertEmoticonIcon />
+        <form>
+          <input
+            type="text"
+            placeholder="Type a Message"
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button onClick={sendMessgae} type="submit">
+            Send a message{" "}
+          </button>
+        </form>
+        <MicIcon />
+      </div>
     </div>
   );
 }
--- whatsapp-clone/src/firebase.js
+++ whatsapp-clone/src/firebase.js
@@ -1,9 +1,18 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAluRMkGdKJF9xtl4t7AB9sVO5UY2MU8xU",
-    authDomain: "whatsappclone-8e081.firebaseapp.com",
-    databaseURL: "https://whatsappclone-8e081.firebaseio.com",
-    projectId: "whatsappclone-8e081",
-    storageBucket: "whatsappclone-8e081.appspot.com",
-    messagingSenderId: "691509940483",
-    appId: "1:691509940483:web:1f6e2924b382a9dc14d936"
-  };
+  apiKey: "AIzaSyAluRMkGdKJF9xtl4t7AB9sVO5UY2MU8xU",
+  authDomain: "whatsappclone-8e081.firebaseapp.com",
+  databaseURL: "https://whatsappclone-8e081.firebaseio.com",
+  projectId: "whatsappclone-8e081",
+  storageBucket: "whatsappclone-8e081.appspot.com",
+  messagingSenderId: "691509940483",
+  appId: "1:691509940483:web:1f6e2924b382a9dc14d936",
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const googleauth = new firebase.auth.GoogleAuthProvider();
+
+export { auth, googleauth };
+export default db;
